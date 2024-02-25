887b5140e7b11112695d89efe89d7d4844d04c1e
Bartek Bachara <bbachara@poczta.onet.pl>
2024-02-25 19:08:18 +0100

    js-hw-11

---
+++ src/script.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+let currentPage = 1;
+let currentSearchQuery = '';
+const form = document.querySelector('#search-form');
+const loadMoreButton = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+loadMoreButton.style.display = 'none';
+
+const fetchData = async (searchQuery, page) => {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=42559721-23019ebdc9c26ab38ac5dea66&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+    );
+    const { hits, totalHits } = response.data;
+    return { hits, totalHits };
+  } catch (error) {
+    console.error('Error fetching data from Pixabay API:', error);
+    throw error;
+  }
+};
+
+const displayImages = (hits, totalHits) => {
+  if (hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    const markupArray = hits.map(
+      ({ webformatURL, likes, views, comments, downloads }) => `
+      <div class="photo-card">
+          <img src="${webformatURL}" alt="" loading="lazy" />
+          <div class="info">
+              <p class="info-item"><b>Likes ${likes}</b></p>
+              <p class="info-item"><b>Views ${views}</b></p>
+              <p class="info-item"><b>Comments ${comments}</b></p>
+              <p class="info-item"><b>Downloads ${downloads}</b></p>
+          </div>
+      </div>`
+    );
+    gallery.innerHTML = gallery.innerHTML + markupArray.join('');
+    if (totalHits > gallery.children.length) {
+      loadMoreButton.style.display = 'block';
+    } else {
+      loadMoreButton.style.display = 'none';
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+    if (currentPage === 1) {
+      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+    }
+  }
+};
+
+const getDataAndDisplayImages = async (searchQuery, page) => {
+  try {
+    const { hits, totalHits } = await fetchData(searchQuery, page);
+    displayImages(hits, totalHits);
+    currentPage = page + 1;
+  } catch (error) {
+    console.error('Error fetching data and displaying images:', error);
+  }
+};
+
+const formSubmit = async event => {
+  event.preventDefault();
+  loadMoreButton.style.display = 'none';
+  const searchQuery = document.querySelector(
+    '#search-form input[name="searchQuery"]'
+  ).value;
+  if (searchQuery !== currentSearchQuery) {
+    currentPage = 1;
+    currentSearchQuery = searchQuery;
+    gallery.innerHTML = '';
+    await getDataAndDisplayImages(searchQuery, currentPage);
+  }
+};
+
+const loadMore = async () => {
+  const searchQuery = document.querySelector(
+    '#search-form input[name="searchQuery"]'
+  ).value;
+  await getDataAndDisplayImages(searchQuery, currentPage);
+};
+
+form.addEventListener('submit', formSubmit);
+loadMoreButton.addEventListener('click', loadMore);
